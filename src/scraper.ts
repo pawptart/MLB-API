b26e02d9d5d0cd5c79aa14cae81e0f8773a5beca
unknown <tyler.b.porter@gmail.com>
2019-06-23 12:30:22 -0500

    Fixed bug with games object

--- src/scraper.ts
+++ src/scraper.ts
@@ -21,6 +21,7 @@ function scrapeHtml(html: any) {
 
 	let games: any = { 
 		date: date,
+		games_played: false,
 		games: []
 	};
 	
@@ -28,101 +29,103 @@ function scrapeHtml(html: any) {
 
 	// Pull data from each game summary 
 	let noGamesPlayed = $('div#content h3').text();
+	games.games_played = !noGamesPlayed;
 
-	$('div.game_summary').each((i, elem) => {
-		let winner = $(elem).find('tr.winner td:not([class]) a').text();
-		let loser = $(elem).find('tr.loser td:not([class]) a').text();
-		let winningScore = $(elem).find('tr.winner td.right').first().text();
-		let losingScore = $(elem).find('tr.loser td.right').first().text();
-		let winningPitcher = $(elem).find('table:not([class]) tbody tr td').eq(1).text();
-		let losingPitcher = $(elem).find('table:not([class]) tbody tr td').eq(3).text();
-		let savePitcher = $(elem).find('table:not([class]) tbody tr td').eq(5).text();
-		let homeTeam = $(elem).find('table.teams tbody tr td a').eq(2).text();
-		let awayTeam = $(elem).find('table.teams tbody tr td a').eq(0).text();
-		let possibleExtraInnings = $(elem).find('tr td.right').text().trim();
+	if (!noGamesPlayed) {
+		$('div.game_summary').each((i, elem) => {
+			let winner = $(elem).find('tr.winner td:not([class]) a').text();
+			let loser = $(elem).find('tr.loser td:not([class]) a').text();
+			let winningScore = $(elem).find('tr.winner td.right').first().text();
+			let losingScore = $(elem).find('tr.loser td.right').first().text();
+			let winningPitcher = $(elem).find('table:not([class]) tbody tr td').eq(1).text();
+			let losingPitcher = $(elem).find('table:not([class]) tbody tr td').eq(3).text();
+			let savePitcher = $(elem).find('table:not([class]) tbody tr td').eq(5).text();
+			let homeTeam = $(elem).find('table.teams tbody tr td a').eq(2).text();
+			let awayTeam = $(elem).find('table.teams tbody tr td a').eq(0).text();
+			let possibleExtraInnings = $(elem).find('tr td.right').text().trim();
 
-		// Extract saves from savePitcher
-		const saveRegex = new RegExp(/\d\d?/g)
-		const savePitcherSaves = savePitcher.match(saveRegex);
-		let saves = ''
-		if (savePitcherSaves) {
-			saves = savePitcherSaves[0];
-		}
+			// Extract saves from savePitcher
+			const saveRegex = new RegExp(/\d\d?/g)
+			const savePitcherSaves = savePitcher.match(saveRegex);
+			let saves = ''
+			if (savePitcherSaves) {
+				saves = savePitcherSaves[0];
+			}
 
-		// Extract record as W / L from winningPitcher && losingPitcher
-		const pitcherRecordRegex = new RegExp(/\d\d?\d?-\d\d?\d?/g);
+			// Extract record as W / L from winningPitcher && losingPitcher
+			const pitcherRecordRegex = new RegExp(/\d\d?\d?-\d\d?\d?/g);
 
-		let winningPitcherRecord = winningPitcher.match(pitcherRecordRegex);
-		let losingPitcherRecord = losingPitcher.match(pitcherRecordRegex);
+			let winningPitcherRecord = winningPitcher.match(pitcherRecordRegex);
+			let losingPitcherRecord = losingPitcher.match(pitcherRecordRegex);
 
-		let winningPitcherWins = '';
-		let winningPitcherLosses = '';
-		let losingPitcherWins = '';
-		let losingPitcherLosses = '';
+			let winningPitcherWins = '';
+			let winningPitcherLosses = '';
+			let losingPitcherWins = '';
+			let losingPitcherLosses = '';
 
-		if (winningPitcherRecord) {
-			winningPitcherWins = winningPitcherRecord[0].split('-')[0];
-			winningPitcherLosses = winningPitcherRecord[0].split('-')[1];
-		}
+			if (winningPitcherRecord) {
+				winningPitcherWins = winningPitcherRecord[0].split('-')[0];
+				winningPitcherLosses = winningPitcherRecord[0].split('-')[1];
+			}
 
-		if (losingPitcherRecord) {
-			losingPitcherWins = losingPitcherRecord[0].split('-')[0];
-			losingPitcherLosses = losingPitcherRecord[0].split('-')[1];
-		}
+			if (losingPitcherRecord) {
+				losingPitcherWins = losingPitcherRecord[0].split('-')[0];
+				losingPitcherLosses = losingPitcherRecord[0].split('-')[1];
+			}
 
-		// Remove record from pitcher name
-		winningPitcher = winningPitcher.slice(0,winningPitcher.indexOf('\(') - 1);
-		losingPitcher = losingPitcher.slice(0,losingPitcher.indexOf('\(') - 1);
-		savePitcher = savePitcher.slice(0,savePitcher.indexOf('\(') - 1);
+			// Remove record from pitcher name
+			winningPitcher = winningPitcher.slice(0,winningPitcher.indexOf('\(') - 1);
+			losingPitcher = losingPitcher.slice(0,losingPitcher.indexOf('\(') - 1);
+			savePitcher = savePitcher.slice(0,savePitcher.indexOf('\(') - 1);
 
-		// Check for extra innings, else default to 9
-		const extraInningsRegex = new RegExp(/\(\d\d?\)/g);
-		let innings = possibleExtraInnings.match(extraInningsRegex);
-		let finalInnings = '9';
-		if (innings) {
-			finalInnings = innings[0].slice(1, ( innings[0].length - 1 ) );
-		}
+			// Check for extra innings, else default to 9
+			const extraInningsRegex = new RegExp(/\(\d\d?\)/g);
+			let innings = possibleExtraInnings.match(extraInningsRegex);
+			let finalInnings = '9';
+			if (innings) {
+				finalInnings = innings[0].slice(1, ( innings[0].length - 1 ) );
+			}
 
-		// Push game data to the games object
-		games.games.push({
-			games_played: !noGamesPlayed,
-			game: {
-				winning_team: { 
-					name: winner,
-					runs: +winningScore,
-					winning_pitcher: {
-						name: winningPitcher,
-						record: {
-							wins: +winningPitcherWins, 
-							losses: +winningPitcherLosses
+			// Push game data to the games object
+			games.games.push({
+				game: {
+					winning_team: { 
+						name: winner,
+						runs: +winningScore,
+						winning_pitcher: {
+							name: winningPitcher,
+							record: {
+								wins: +winningPitcherWins, 
+								losses: +winningPitcherLosses
+							}
+						},
+						save_pitcher: {
+							name: savePitcher,
+							saves: +saves
 						}
 					},
-					save_pitcher: {
-						name: savePitcher,
-						saves: +saves
-					}
-				},
-				losing_team: {
-					name: loser,
-					runs: +losingScore,
-					losing_pitcher: {
-						name: losingPitcher,
-						record: {
-							wins: +losingPitcherWins,
-							losses: +losingPitcherLosses
+					losing_team: {
+						name: loser,
+						runs: +losingScore,
+						losing_pitcher: {
+							name: losingPitcher,
+							record: {
+								wins: +losingPitcherWins,
+								losses: +losingPitcherLosses
+							}
 						}
-					}
-				},
-				played_at: {
-					home_team: homeTeam,
-					away_team: awayTeam,
-					location: ballparks[homeTeam]
-				},
-				total_innings: +finalInnings
-			}
+					},
+					played_at: {
+						home_team: homeTeam,
+						away_team: awayTeam,
+						location: ballparks[homeTeam]
+					},
+					total_innings: +finalInnings
+				}
+			});
 		});
-	});
-
+	}
+	
 	// Define postData to post to MongoDB
 	let postData = async (req: any) => {
 		const { mongoDb, mongoClient } = await mongoConnect(mongoUrl, 'game');
